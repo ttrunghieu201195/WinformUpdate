649364b09616d5bb3a2008081fcd07dbe4d315bd
Tran Trung Hieu <hieutt.blu@vnpt.vn>
2022-08-23 14:00:57 +0700

    update publish

--- CheckUpdate/CheckUpdate/Form1.Designer.cs
+++ CheckUpdate/CheckUpdate/Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace CheckUpdate
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "1.0.0";
+            this.textBox1.Text = "1.0.1";
             // 
             // Form1
             // 
